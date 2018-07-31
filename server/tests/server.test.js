314667d29ded17834d4476faa320ebebd5218115
adri <adri.skrtic@gmail.com>
2018-07-31 15:36:46 -0400

    upgraded expect from 1 to 21

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -20,14 +20,14 @@ describe ('POST /users/login', ()=>{
             })
             .expect(200)
             .expect((res)=>{
-                expect(res.headers['x-auth']).toExist();
+                expect(res.headers['x-auth']).toBeTruthy();
             })
             .end((err, res)=>{
                 if (err) {
                     return done(err);
                 }
                 User.findById(users[1]._id).then((user)=>{
-                    expect(user.tokens[1]).toInclude({
+                    expect(user.toObject().tokens[1]).toMatchObject({
                         access:'auth',
                         token:res.headers['x-auth']
                     })
@@ -108,16 +108,16 @@ describe ('/POST users', ()=>{
         .send({email, password})
         .expect(200)
         .expect((res)=>{
-            expect(res.headers['x-auth']).toExist();
+            expect(res.headers['x-auth']).toBeTruthy();
             expect(res.body.email).toBe(email);
         })
         .end((err, res)=>{
             if (err) {
                 return done(err);
             }
-            User.find({email}).then((users)=>{
-                console.log(`count: ${users.length}`);
-                expect(users.length).toBe(1);
+            User.findOne({email}).then((user)=>{
+                expect(user).toBeTruthy();
+                expect(user.password).not.toBe(password);
                 done();
             }).catch((err)=>{
                 done(err);
@@ -271,7 +271,7 @@ describe('DELETE /todos:id', ()=>{
                 return done(err);
             }
             Todo.findById(hexId).then((todo)=>{
-                expect(todo).toBe(null);
+                expect(todo).toBeFalsy();
                 done();
             }).catch((err)=>{done(err);});
         });
@@ -315,7 +315,9 @@ describe('PATCH /todos/id', ()=>{
             .expect((res)=>{
                 expect(res.body.todo.completed).toBe(true)
                 expect(res.body.todo.text).toBe(text);
-                expect(res.body.todo.completedAt).toBeA('number');
+                expect(typeof res.body.todo.completedAt).toBe('number');
+//                expect(res.body.todo.completedAt).toBeA('number');
+
             })
             .end((err, req)=>{
                 if (err) {
